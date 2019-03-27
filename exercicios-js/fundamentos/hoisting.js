eb58e042f2bb3be5241452a4ba6ee0f3b813ca83
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2019-03-27 01:18:53 -0300

    Add files via upload

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,8 @@
+console.log("a = " + a)
+var a = 2                 //a variavel "a" é movida para cima para pode ser lida
+console.log("a = " + a)
+
+
+console.log("a = " + b)
+let b = 2                 //a variavel b não é lida, gerando um erro ao tentar mostra-la
+console.log("a = " + b)
